ee360526da989aab5b76027d53d98bad91ee9b2e
Marina Sokolova <sokolova.m.e9@gmail.com>
2020-05-07 22:47:08 +0300

    create Logger

---
+++ 2.C#Fundamentals/CSharpFundamentals/BCL/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BCL
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
