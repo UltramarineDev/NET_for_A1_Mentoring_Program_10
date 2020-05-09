5b6505e8ac5d0066d4959a83e3a8119b657ac620
Marina Sokolova <sokolova.m.e9@gmail.com>
2020-05-09 21:01:19 +0300

    create Task3 sln.

---
+++ 3.BasicSQL/Task3/Task3-v1.1/dbo/Tables/CreditCardDetails.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CreditCardDetails]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [CardNumber] INT NOT NULL, 
+    [ExpiredDate] DATETIME NULL, 
+    [CardHolder] NVARCHAR(50) NULL, 
+    [EmployeeID] INT REFERENCES Employees(EmployeeID) NOT NULL
+)
