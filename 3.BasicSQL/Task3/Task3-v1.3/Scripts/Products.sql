818884c1fd2bc87525484b952762d01ffad75139
Marina Sokolova <sokolova.m.e9@gmail.com>
2020-05-09 22:29:21 +0300

    create post deployment script - task 3.3

---
+++ 3.BasicSQL/Task3/Task3-v1.3/Scripts/Products.sql
@@ -0,0 +1,83 @@
+﻿set identity_insert  Products on
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "ProductID" FROM Products WHERE "ProductID" = 1)
+   BEGIN
+	INSERT  Products ("ProductID","ProductName","SupplierID","CategoryID","QuantityPerUnit","UnitPrice","UnitsInStock","UnitsOnOrder","ReorderLevel","Discontinued") VALUES(1,'Chai',1,1,'10 boxes x 20 bags',18,39,0,10,0)
+   END
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "ProductID" FROM Products WHERE "ProductID" = 2)
+   BEGIN
+	INSERT  Products ("ProductID","ProductName","SupplierID","CategoryID","QuantityPerUnit","UnitPrice","UnitsInStock","UnitsOnOrder","ReorderLevel","Discontinued") VALUES(2,'Chang',1,1,'24 - 12 oz bottles',19,17,40,25,0)
+   END
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "ProductID" FROM Products WHERE "ProductID" = 3)
+   BEGIN
+	INSERT  Products ("ProductID","ProductName","SupplierID","CategoryID","QuantityPerUnit","UnitPrice","UnitsInStock","UnitsOnOrder","ReorderLevel","Discontinued") VALUES(3,'Aniseed Syrup',1,2,'12 - 550 ml bottles',10,13,70,25,0)
+   END
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "ProductID" FROM Products WHERE "ProductID" = 4)
+   BEGIN
+	INSERT  Products ("ProductID","ProductName","SupplierID","CategoryID","QuantityPerUnit","UnitPrice","UnitsInStock","UnitsOnOrder","ReorderLevel","Discontinued") VALUES(4,'Chef Anton''s Cajun Seasoning',2,2,'48 - 6 oz jars',22,53,0,0,0)
+   END
+
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "ProductID" FROM Products WHERE "ProductID" = 5)
+   BEGIN
+	INSERT Products ("ProductID","ProductName","SupplierID","CategoryID","QuantityPerUnit","UnitPrice","UnitsInStock","UnitsOnOrder","ReorderLevel","Discontinued") VALUES(5,'Chef Anton''s Gumbo Mix',2,2,'36 boxes',21.35,0,0,0,1)
+   END
+
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "ProductID" FROM Products WHERE "ProductID" = 6)
+   BEGIN
+	INSERT  Products ("ProductID","ProductName","SupplierID","CategoryID","QuantityPerUnit","UnitPrice","UnitsInStock","UnitsOnOrder","ReorderLevel","Discontinued") VALUES(6,'Grandma''s Boysenberry Spread',3,2,'12 - 8 oz jars',25,120,0,25,0)
+   END
+
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "ProductID" FROM Products WHERE "ProductID" = 7)
+   BEGIN
+	INSERT  Products ("ProductID","ProductName","SupplierID","CategoryID","QuantityPerUnit","UnitPrice","UnitsInStock","UnitsOnOrder","ReorderLevel","Discontinued") VALUES(7,'Uncle Bob''s Organic Dried Pears',3,7,'12 - 1 lb pkgs.',30,15,0,10,0)
+   END
+
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "ProductID" FROM Products WHERE "ProductID" = 8)
+   BEGIN
+	INSERT  Products ("ProductID","ProductName","SupplierID","CategoryID","QuantityPerUnit","UnitPrice","UnitsInStock","UnitsOnOrder","ReorderLevel","Discontinued") VALUES(8,'Northwoods Cranberry Sauce',3,2,'12 - 12 oz jars',40,6,0,0,0)
+   END
+
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "ProductID" FROM Products WHERE "ProductID" = 9)
+   BEGIN
+	INSERT  Products ("ProductID","ProductName","SupplierID","CategoryID","QuantityPerUnit","UnitPrice","UnitsInStock","UnitsOnOrder","ReorderLevel","Discontinued") VALUES(9,'Mishi Kobe Niku',4,6,'18 - 500 g pkgs.',97,29,0,0,1)
+   END
+
+END
+GO
+
+set identity_insert Products off
+GO
---
+++ 3.BasicSQL/Task3/Task3-v1.3/Scripts/Supplies.sql
@@ -0,0 +1,61 @@
+﻿set identity_insert Suppliers on
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "SupplierID" FROM Suppliers WHERE "SupplierID" = 1)
+   BEGIN
+	INSERT  Suppliers ("SupplierID","CompanyName","ContactName","ContactTitle","Address","City","Region","PostalCode","Country","Phone","Fax","HomePage") VALUES(1,'Exotic Liquids','Charlotte Cooper','Purchasing Manager','49 Gilbert St.','London',NULL,'EC1 4SD','UK','(171) 555-2222',NULL,NULL)
+   END
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "SupplierID" FROM Suppliers WHERE "SupplierID" = 2)
+   BEGIN
+	INSERT  Suppliers ("SupplierID","CompanyName","ContactName","ContactTitle","Address","City","Region","PostalCode","Country","Phone","Fax","HomePage") VALUES(2,'New Orleans Cajun Delights','Shelley Burke','Order Administrator','P.O. Box 78934','New Orleans','LA','70117','USA','(100) 555-4822',NULL,'#CAJUN.HTM#')
+   END
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "SupplierID" FROM Suppliers WHERE "SupplierID" = 3)
+   BEGIN
+	INSERT  Suppliers ("SupplierID","CompanyName","ContactName","ContactTitle","Address","City","Region","PostalCode","Country","Phone","Fax","HomePage") VALUES(3,'Grandma Kelly''s Homestead','Regina Murphy','Sales Representative','707 Oxford Rd.','Ann Arbor','MI','48104','USA','(313) 555-5735','(313) 555-3349',NULL)
+   END
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "SupplierID" FROM Suppliers WHERE "SupplierID" = 4)
+   BEGIN
+	INSERT  Suppliers ("SupplierID","CompanyName","ContactName","ContactTitle","Address","City","Region","PostalCode","Country","Phone","Fax","HomePage") VALUES(4,'Tokyo Traders','Yoshi Nagase','Marketing Manager','9-8 Sekimai Musashino-shi','Tokyo',NULL,'100','Japan','(03) 3555-5011',NULL,NULL)
+   END
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "SupplierID" FROM Suppliers WHERE "SupplierID" = 5)
+   BEGIN
+	INSERT  Suppliers ("SupplierID","CompanyName","ContactName","ContactTitle","Address","City","Region","PostalCode","Country","Phone","Fax","HomePage") VALUES(5,'Cooperativa de Quesos ''Las Cabras''','Antonio del Valle Saavedra','Export Administrator','Calle del Rosal 4','Oviedo','Asturias','33007','Spain','(98) 598 76 54',NULL,NULL)
+   END
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "SupplierID" FROM Suppliers WHERE "SupplierID" = 6)
+   BEGIN
+	INSERT  Suppliers ("SupplierID","CompanyName","ContactName","ContactTitle","Address","City","Region","PostalCode","Country","Phone","Fax","HomePage") VALUES(6,'Mayumi''s','Mayumi Ohno','Marketing Representative','92 Setsuko Chuo-ku','Osaka',NULL,'545','Japan','(06) 431-7877',NULL,'Mayumi''s (on the World Wide Web)#http://www.microsoft.com/accessdev/sampleapps/mayumi.htm#')
+   END
+END
+GO
+
+BEGIN
+   IF NOT EXISTS (SELECT "SupplierID" FROM Suppliers WHERE "SupplierID" = 7)
+   BEGIN
+	INSERT  Suppliers ("SupplierID","CompanyName","ContactName","ContactTitle","Address","City","Region","PostalCode","Country","Phone","Fax","HomePage") VALUES(7,'Pavlova, Ltd.','Ian Devling','Marketing Manager','74 Rose St. Moonie Ponds','Melbourne','Victoria','3058','Australia','(03) 444-2343','(03) 444-6588',NULL)
+   END
+END
+GO
+
+set identity_insert Suppliers off
+GO
