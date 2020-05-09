30567bae10b751becf4c471bf4cc8c00ec376aee
Marina Sokolova <sokolova.m.e9@gmail.com>
2020-05-09 23:15:19 +0300

    remove SSDTUsageTask3_2 project(replace with new one)

--- 3.BasicSQL/SSDTUsageTask3_2/DatabaseProject/dbo/Tables/CreditCardDetails.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[CreditCardDetails]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [ExspirationDate] DATETIME NULL, 
-    [EmployeeId] INT NULL,
-	FOREIGN KEY (EmployeeId) REFERENCES Employees(EmployeeId)
-)
--- 3.BasicSQL/SSDTUsageTask3_2/DatabaseProject/dbo/Tables/CreditDetails.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[CreditDetails] (
-    [CreditID]       INT           IDENTITY (1, 1) NOT NULL,
-    [CustomerID]     NCHAR (5)     NULL,
-    [CardNumber]     INT           NOT NULL,
-    [ExpirationDate] DATETIME      NULL,
-    [CardHolderName] NVARCHAR (40) NULL,
-    FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([CustomerID])
-);
-
